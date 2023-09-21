3bdde0829d6cc68a54d0bbbed64a1d8860385e50
tovehydmark <tovehydmark@gmail.com>
2023-09-21 17:00:21 +0200

    Order data interface

---
+++ models/Order.ts
@@ -0,0 +1,12 @@
+export interface Order {
+  orderId: string;
+  productId: string;
+  currency: string;
+  quantity: string;
+  shippingCost: string;
+  amount: string;
+  channel: string;
+  channelGroup: string;
+  campaign: string;
+  dateTime: string;
+}
